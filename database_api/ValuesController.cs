818e5d54814f5c4f032ec3019e36fc6e93b2cbd7
Quang3295 <94293232+Quang3295@users.noreply.github.com>
2023-05-17 20:35:17 +0700

    test_push

--- database_api/ValuesController.cs
+++ database_api/ValuesController.cs
@@ -888,11 +888,7 @@ on notifications.BoardID=lists.BoardID";
             return Ok(ex.Message);
         }
     }
-   [Route("api/test/{keyword}")]
-   public IHttpActionResult Test(string keyword)
-   {
-     
-   }
+    
     [Route("api/searchLists/{keyword}")]
     public IHttpActionResult SearchLists(string keyword)
     {
